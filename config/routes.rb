a9b24b77bf26475cd21eceaf48f877555dda403f
takumi-shibata <241448ta@gmail.com>
2021-06-29 09:46:22 +0000

    投稿機能4つ実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get "posts/index" => "posts#index"
+  get "posts/new" => "posts#new"
+  post "posts/create" => "posts#create"
+  get "posts/:id" => "posts#show"
+  get 'posts/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
